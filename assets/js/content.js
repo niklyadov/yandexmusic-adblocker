8ccce7c7f8a471f08e1081d437a8a5ea0237499f
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2020-03-07 01:00:12 +0500

    rev 0.6

--- assets/js/content.js
+++ assets/js/content.js
@@ -4,8 +4,98 @@
     //github.com/Anchovys/yandexmusic-adblocker
 */
 
-var elem = document.createElement('div');
-elem.id = "anc__ya-m_ad-blocker_loaded";
-elem.innerHTML = "YaMusic AD-blocker loaded!";
+window.addEventListener("load",  init, false);
+window.addEventListener("click", onWindowClick );
 
-document.body.appendChild(elem);
+var data = null; 
+var lastInteract = Date.now();
+var isPlaying = false;
+
+function init() 
+{
+    data = getData(); // getting data about player
+
+    if (data == null) // is null (run init loop)
+    { 
+        console.log("[yandex ad blocker] Can't init extension");
+        setTimeout(init, 500); // looping 500 ms
+        return;
+    }
+    else // success init
+    {
+        console.log("[yandex ad blocker] Success inited extension!");
+        
+        placeElement();
+        sendMsgPopup();
+        
+        atrTracking(document.getElementsByTagName("body")[0]);
+
+    }
+}
+
+/*
+function sendMsgPopup () 
+{
+    chrome.runtime.sendMessage(
+    {
+        data: "Loaded"
+    }, function (response)
+    {
+        console.dir(response);
+    });
+}
+*/
+
+function onWindowClick(event)
+{
+    lastInteract = Date.now();
+}
+
+function atrTracking(element) 
+{
+    var observer = new MutationObserver(function(mutations)
+    {
+        mutations.forEach(function(mutation)
+        {
+            if (mutation.type == "attributes")
+            {
+                data = getData(); // update data to actual
+                var diffInteractTime = (Date.now() - lastInteract) / 60000;
+
+                console.log("[yandex ad blocker] MUTATE -- obj:" + JSON.stringify(data) + " -- t:" + diffInteractTime);
+
+                // > 10 minutes no interact
+                if(diffInteractTime > 10 && data.playing == false && isPlaying)
+                {
+                    setTimeout(function () {
+                        document.getElementsByClassName("button-play")[0].click();
+                        lastInteract = Date.now();
+                    }, 200);
+                }
+
+                isPlaying = data.playing;
+            }
+        });
+    });
+    
+    observer.observe(element,
+    {
+        attributes: true
+    });
+}
+
+function placeElement()
+{
+    var elem = document.createElement('div');
+        elem.id = "anc__ya-m_ad-blocker_loaded";
+        elem.innerHTML = "YaMusic AD-blocker loaded!";
+    document.body.appendChild(elem);
+}
+
+function getData() 
+{
+    var jsonData = document.getElementsByTagName("body")[0].getAttribute("data-unity-state");
+
+    var obj = JSON.parse(jsonData);
+    return obj;
+}
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -4,12 +4,26 @@
     //github.com/Anchovys/yandexmusic-adblocker
 */
 var background = chrome.extension.getBackgroundPage();
+var loaded = true;
 
 function onError(error) 
 {
     console.error(`Error: ${error}`);
 }
 
+/*
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse)
+{
+    if(message.data == "Loaded")
+    {
+        loaded = true;
+    }
+    
+    sendResponse({
+        data: "Ok"
+    }); 
+});
+*/
 document.addEventListener('DOMContentLoaded', function()
 {
     chrome.tabs.getSelected(null, function(tab)
@@ -19,9 +33,18 @@ document.addEventListener('DOMContentLoaded', function()
 
         if (background.correctUrlRegex.test(tab.url)) // check is url correct
         {
-            statusBar.style.borderColor = '#6cd66c';
-            statusBar.innerHTML = 'Works pretty.</b><br>But, if you have a problems, reload the page!';
-            statusImg.src = '../img/facegreen.png';
+            if(loaded)
+            {
+                statusBar.style.borderColor = '#6cd66c';
+                statusBar.innerHTML = 'Works pretty.</b><br>But, if you have a problems, reload the page!';
+                statusImg.src = '../img/facegreen.png';
+            } else 
+            {
+                statusBar.style.borderColor = '#6cd66c';
+                statusBar.innerHTML = 'Some problems.</b><br>Have a some problems with page!';
+                statusImg.src = '../img/faceyellow.png';
+            }
+
         } else
         {
             statusBar.style.borderColor = 'darkred';
